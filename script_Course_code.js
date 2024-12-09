892d3b23e2634293cf31e9c3f4445222db762450
S.Hosein Hoseini <s.hosein.h.82@gmail.com>
2024-12-09 21:49:08 +0330

    Update course code

--- script_Course_code.js
+++ script_Course_code.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Select elements
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -32,11 +34,30 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
     currentScore = 0;
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
   }
+});
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active player's
+  document.getElementById(`score--${activePlayer}`).textContent = currentScore;
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  // console.log(scores);
+  // 2. if Check if player's score is >= 100
+  document.getElementById(`score--${activePlayer}`).textContent >= 100
+    ? btnHold.removeEventListener('click')
+    : //  btnNew.removeEventListener('click')
+      // 3. swirch player
+      (activePlayer = activePlayer === 0 ? 1 : 0),
+    player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
 });
